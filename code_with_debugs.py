aa1a734cae52df7688b478eaada6e739ff67ac73
erezhu <erez1816@gmail.com>
2025-03-16 09:39:57 +0200

    Update code_with_debugs.py

--- code_with_debugs.py
+++ code_with_debugs.py
@@ -11,7 +11,7 @@ import glob
 
 # Configure logging
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.WARNING,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
         logging.FileHandler("autosam2.log"),
